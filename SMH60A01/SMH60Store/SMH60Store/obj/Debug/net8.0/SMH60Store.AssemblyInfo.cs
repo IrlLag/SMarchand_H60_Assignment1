813656c46279729ba68f98f9f715ebd1615f787a
Spencer Marchand <spencerdmarchand@gmail.com>
2026-09-24 23:32:35 -0400

    Add CSS for _Layout/cshtml

--- SMH60A01/SMH60Store/SMH60Store/obj/Debug/net8.0/SMH60Store.AssemblyInfo.cs
+++ SMH60A01/SMH60Store/SMH60Store/obj/Debug/net8.0/SMH60Store.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SMH60Store")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05831f122b08ec8eb5b9676f3cc7ac854f2bd84f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31af2450a739f93ee8fb8066081b5fa85bf03dad")]
 [assembly: System.Reflection.AssemblyProductAttribute("SMH60Store")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SMH60Store")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
